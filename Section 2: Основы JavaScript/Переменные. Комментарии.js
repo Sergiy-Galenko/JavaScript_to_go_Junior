c2c195053bfea754d57a0ebefc06380251d2a935
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-07-19 21:59:26 +0300

    Variables. Comments

---
+++ Section 2: Основы JavaScript/Переменные. Комментарии.js
@@ -0,0 +1,24 @@
+// var
+
+// let
+
+//const
+
+var name = 'Jack';
+console.log(name);
+name = 'Jane';
+console.log(name);
+var color;
+console.log(color); // undefined
+color = 'red';
+console.log(color); 
+
+
+// JavaScript variables names can includes
+// letters, numbers, $, _
+// JavaScript variables names can`t start with number
+
+myMainColor = 'green';//camel case
+my_main_color //snake case
+my-main-color // kebab case
+MyMainCase // Pascal case
