fad8c0627f8d19a5b28a345c79efa5efaa5e72b1
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-08-04 21:58:54 +0300

    Create  Простые Методы Массивов.js

---
+++ Section 9: Работа С Массивами/ Простые Методы Массивов.js
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////
+// Простые Методы Массивов
+
+let letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// slice()
+
+console.log(letters.slice(1));
+console.log(letters.slice(1, 4));
+console.log(letters.slice(-1));
+console.log(letters.slice(-3));
+console.log(letters.slice(-3, -1));
+console.log(letters.slice(2, -1));
+console.log(letters.slice());
+console.log([...letters]);
+
+//splice()
+
+console.log(letters.splice(1));
+letters.splice(-1);
+letters.splice(1, 3);
+
+let letters1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+let letters2 = ['g', 'h', 'i'];
+
+// reverse()
+
+console.log(letters.reverse());
+
+// concat()
+
+// const letters = letters1.concat(letters2);
+// console.log(letters);
+// console.log([...letters1, ...letters2]);
+
+// join()
+
+// console.log(letters.join(', '));
