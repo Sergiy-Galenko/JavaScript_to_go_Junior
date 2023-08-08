19ee1ecfd78b7f65d06315adf307ecb43fda679c
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-08-08 21:31:13 +0300

    Create script.js

---
+++ Section 9: Работа С Массивами/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['CNY', 'Chinese yuan'],
+]);
+
+const transactions = [300, 250, -500, 5000, -750, -180, 50, 1400, -350, -120];
+
+
+
+
