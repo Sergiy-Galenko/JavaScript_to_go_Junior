434cbfde8981e3fb6f3e718679ff2ced87923776
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-08-09 17:31:22 +0300

    Create Интернационализация Чисел.js

---
+++ Section 10: Числа, Даты, Таймеры/Lectures/Интернационализация Чисел.js
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////
+//Интернационализация Чисел
+
+const a = 325458478.21;
+
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+};
+
+console.log('US', new Intl.NumberFormat('en-US', options).format(a));
+console.log('Ukraine', new Intl.NumberFormat('uk-UA', options).format(a));
+console.log('Russia', new Intl.NumberFormat('ru-RU', options).format(a));
+console.log('Germany', new Intl.NumberFormat('de-DE', options).format(a));
+console.log('Syria', new Intl.NumberFormat('ar-SY', options).format(a));
