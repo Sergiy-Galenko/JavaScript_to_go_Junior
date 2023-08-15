c75c2afa071f3e5d1ba0401a878c7753d4ace75c
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-08-15 12:13:29 +0300

    Create Пример работы с циклом событий.js

---
+++ Section 14: Асинхронный JavaScript - Promises, Async/Пример работы с циклом событий.js
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////
+//Пример работы с циклом событий
+
+console.log('Начало теста');
+setTimeout(() => console.log('Таймер 0 секунд'), 0);
+Promise.resolve('Выполненное promise 1').then(result => console.log(result));
+Promise.resolve('Выполненное promise 2').then(result => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log(result);
+});
+console.log('Конец теста');
